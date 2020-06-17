5a266eca43d3a0ed245f97c3ed7606467962470e
makubex64 <jorgeluismakubex@gmail.com>
2020-06-17 16:54:48 -0400

    add files

---
+++ js/app.js
@@ -0,0 +1,66 @@
+
+  
+    sr = ScrollReveal();
+
+    sr.reveal('.collapse', {
+      duration: 800,
+      origin: 'top',
+      distance: '150px',
+      delay: 100
+    });
+
+    sr.reveal('.navbar', {
+      duration: 400,
+      origin: 'bottom',
+      delay: 120
+    });
+
+    sr.reveal('.carousel-inner', {
+      duration: 1200,
+      origin: 'top',
+      distance: '10px',
+      delay: 50
+    });
+
+    sr.reveal('.line', {
+      duration: 1200,
+      distance: '300px',
+      origin: 'right',
+      delay: 50
+
+    });
+
+    sr.reveal('.bg-equipo', {
+      duration: 1200,
+      distance: '300px',
+      origin: 'right',
+      delay: 50
+    });
+
+    sr.reveal('.we-are', {
+      duration: 1200,
+      distance: '300px',
+      origin: 'right',
+      delay: 50
+
+    });
+
+    sr.reveal('.noticia-1', {
+      duration: 1200,
+      distance: '300px',
+      origin: 'right',
+      delay: 50
+    });
+
+   sr.reveal('.noticia-2', {
+      duration: 1200,
+      distance: '300px',
+      origin: 'left',
+      delay: 50
+    })
+   sr.reveal('.noticia-3', {
+      duration: 1200,
+      distance: '300px',
+      origin: 'right',
+      delay: 50
+    });
