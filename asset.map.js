914144a595c00fe3cc1a4621da17a8dec1d3266c
Luis Jorge Adarmes Rodriguez <jorgeluismakubex@gmail.com>
2020-10-11 10:30:28 -0400

    add files others

---
+++ asset.map.js
@@ -0,0 +1,11 @@
+const css [
+	'node_modules/bootstrap/dist/css/bootstrap.min.css'
+]
+
+const js [
+	'node_modules/bootstrap/dist/js/bootstrap.min.js',
+	'node_modules/jquery/dist/jquery.min.js',
+	'node_modules/jquery.easing/jquery.easing.min.js'
+]
+
+module.exports = [...css, ...js]
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const assetMap = require('asset.map');
+
+module.exports = {
+  plugins: [
+    new CopyPlugin({
+      patterns: assetMap.map(item =>{
+        	return {
+        			from: path.resolve(__dirname, `${item}`)
+        	}
+        })
+      
+    }),
+  ],
+};
