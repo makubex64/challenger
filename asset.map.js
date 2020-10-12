c05f2267e2d02b56dafe0d2765ac5c704acfae4d
Luis Jorge Adarmes Rodriguez <jorgeluismakubex@gmail.com>
2020-10-12 19:16:55 -0400

    add filess

--- asset.map.js
+++ asset.map.js
@@ -1,11 +1,3 @@
-const css [
-	'node_modules/bootstrap/dist/css/bootstrap.min.css'
-]
-
-const js [
-	'node_modules/bootstrap/dist/js/bootstrap.min.js',
-	'node_modules/jquery/dist/jquery.min.js',
-	'node_modules/jquery.easing/jquery.easing.min.js'
 const  css = [
 	'bootstrap/dist/css/bootstrap.min.css'
 ]
--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
  // navbar smothScroll
- document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+//  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// shrink navbar on scroll
+// window.onscroll = function() {
+//     var scrollTop = $(window).scrollTop();
+//     var navTop = document.querySelector('.navTop')
+//     navTop.innerHTML = 'el escroll es:' + scrollTop
+// };
+
+var menu = document.querySelector('#menu');
+var MenuTop = menu.offsetTop;
+
+var myFunction = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if(scrollTop > 100){
+        menu.style.padding = "22px"        
+    }else{
+         menu.style.padding = ""
+    }
+}
+window.onscroll = function(){myFunction()};
 
 
  
@@ -15,8 +36,11 @@
  var pro = function(){
 	const image = document.querySelectorAll('.carousel-item img');
 	image.forEach(item=>{
-		item.style.height = "500px"
+		item.style.height = "690px"
+        item.style.maxHeight = "100%"
+        item.className += " img-fluid"
 	})
+
 
 }
 pro()
